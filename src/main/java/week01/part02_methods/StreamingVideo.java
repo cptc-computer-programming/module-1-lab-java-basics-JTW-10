26de824d029557545f0f2840db6c988793c591b3
Jay Goulbourne <83363676+JTW-10@users.noreply.github.com>
2026-07-02 14:32:56 -0700

    Removed TODO from StreamingVideo

--- src/main/java/week01/part02_methods/StreamingVideo.java
+++ src/main/java/week01/part02_methods/StreamingVideo.java
@@ -7,42 +7,25 @@ public class StreamingVideo {
         playVideo();
     }
 
-    // TODO: main must call this method.
-    // This method must call:
-    //   1) loadVideo()
-    //   2) startPlayback()
-    // and startPlayback() must call:
-    //   - showAd()
-    //   - beginStream()
-    //
-    // Print exactly:
-    // Play video selected.
-    // Loading video.
-    // Showing ad.
-    // Video is now playing.
     public static void playVideo() {
         System.out.println("Play video selected.");
         loadVideo();
         startPlayback();
     }
 
-    // TODO: Print exactly: Loading video.
     public static void loadVideo() {
         System.out.println("Loading video.");
     }
 
-    // TODO: Must call showAd() and beginStream()
     public static void startPlayback() {
         showAd();
         beginStream();
     }
 
-    // TODO: Print exactly: Showing ad.
     public static void showAd() {
         System.out.println("Showing ad.");
     }
 
-    // TODO: Print exactly: Video is now playing.
     public static void beginStream() {
         System.out.println("Video is now playing.");
     }
